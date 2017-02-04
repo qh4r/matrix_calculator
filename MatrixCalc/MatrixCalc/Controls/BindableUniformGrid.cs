60ea2c96a0c82e86e0b2cc27822a0239d5943482
Rafa� Kucharski <rafal.kucharski@ac-project.net>
2017-02-04 20:08:26 +0100

    Added matrixes related logic

---
+++ MatrixCalc/MatrixCalc/Controls/BindableUniformGrid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalc.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls.Primitives;
+    public class BindableUniformGrid : UniformGrid
+    {
+
+
+        public int RowsCount
+        {
+            get { return (int)GetValue(RowsCountProperty); }
+            set { SetValue(RowsCountProperty, value); }
+        }
+
+        public static readonly DependencyProperty RowsCountProperty =
+            DependencyProperty.Register("RowsCount", typeof(int), typeof(BindableUniformGrid), new PropertyMetadata(1, OnRowsCountChange));
+
+        private static void OnRowsCountChange(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var grid = dependencyObject as BindableUniformGrid;
+            var newCount = dependencyPropertyChangedEventArgs.NewValue as int?;
+            if (newCount.HasValue && grid != null)
+            {
+                grid.Rows = newCount.Value;
+            }
+        }
+
+        public int ColumnsCount
+        {
+            get { return (int)GetValue(ColumnsCountProperty); }
+            set { SetValue(ColumnsCountProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColumnsCountProperty =
+            DependencyProperty.Register("ColumnsCount", typeof(int), typeof(BindableUniformGrid), new PropertyMetadata(1, OnColumnsCountChange));
+
+        private static void OnColumnsCountChange(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var grid = dependencyObject as BindableUniformGrid;
+            var newCount = dependencyPropertyChangedEventArgs.NewValue as int?;
+            if (newCount.HasValue && grid != null)
+            {
+                grid.Columns = newCount.Value;
+            }
+        }
+    }
+}
---
+++ MatrixCalc/MatrixLibrary/Matrix.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    using System.Collections;
+
+    public class Matrix : IEnumerable<double>
+    {
+        private readonly double[,] matrix;
+
+        public Matrix(int rowsCount, int columnsCount)
+        {
+            this.RowsCount = rowsCount;
+            this.ColumnsCount = columnsCount;
+            this.matrix = new double[rowsCount, columnsCount];
+        }
+
+        public Matrix(int rowsCount, int columnsCount, IEnumerable<double> values) : this(rowsCount, columnsCount)
+        {
+            var valuesEnumerator = values.GetEnumerator();
+            for (var i = 0; i < this.RowsCount; i++)
+            {
+                for (var j = 0; j < this.ColumnsCount; j++)
+                {
+                    var next = valuesEnumerator.MoveNext();
+                    this.matrix[i, j] = next ? valuesEnumerator.Current : 0;
+                }
+            }
+        }
+
+        public Matrix(int rowsCount, int columnsCount, Matrix matrix) : this(rowsCount, columnsCount)
+        {
+            for (var i = 0; i < this.RowsCount; i++)
+            {
+                for (var j = 0; j < this.ColumnsCount; j++)
+                {
+                    this.matrix[i, j] = (i < matrix.RowsCount && j < matrix.ColumnsCount) ? matrix[i, j] : 0;
+                }
+            }
+        }
+
+        public double this[int iRow, int iCol]
+        {
+            get { return this.matrix[iRow, iCol]; }
+            set { this.matrix[iRow, iCol] = value; }
+        }
+
+        public int RowsCount { get; }
+
+        public int ColumnsCount { get; }
+
+        public void SetColumn(int columnNumber, IEnumerable<double> elements)
+        {
+            var newValues = elements as double[] ?? elements.ToArray();
+            if (this.RowsCount != newValues.Count())
+            {
+                throw new Exception("Wrong number of items");
+            }
+            if (columnNumber >= this.ColumnsCount)
+            {
+                throw new Exception("Columns number exceeds existing columns");
+            }
+            for (int i = 0; i < this.RowsCount; i++)
+            {
+                this.matrix[i, columnNumber] = newValues[i];
+            }
+        }
+
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix1.RowsCount != matrix2.RowsCount || matrix1.ColumnsCount != matrix2.ColumnsCount) throw new Exception("Matrix dimensions must match!");
+            var r = new Matrix(matrix1.RowsCount, matrix1.ColumnsCount);
+            for (var i = 0; i < r.RowsCount; i++)
+                for (var j = 0; j < r.RowsCount; j++)
+                    r[i, j] = matrix1[i, j] + matrix2[i, j];
+            return r;
+        }
+
+        public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix1.RowsCount != matrix2.RowsCount || matrix1.ColumnsCount != matrix2.ColumnsCount) throw new Exception("Matrix dimensions must match!");
+            var r = new Matrix(matrix1.RowsCount, matrix1.ColumnsCount);
+            for (var i = 0; i < r.RowsCount; i++)
+                for (var j = 0; j < r.RowsCount; j++)
+                    r[i, j] = matrix1[i, j] - matrix2[i, j];
+            return r;
+        }
+
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix2.RowsCount != matrix1.ColumnsCount)
+            {
+                throw new Exception("Matrixes can not be multiplied");
+            }
+            var result = new Matrix(matrix1.RowsCount, matrix2.ColumnsCount);
+
+            for (var i = 0; i < matrix1.RowsCount; i++)
+            {
+                for (var j = 0; j < matrix2.ColumnsCount; j++)
+                {
+                    double tmp = 0;
+                    for (var k = 0; k < matrix2.RowsCount; k++)
+                    {
+                        tmp += matrix1[i, k] * matrix1[k, j];
+                    }
+                    result[i, j] = tmp;
+                }
+            }
+            return result;
+        }
+
+        public void SetRow(int rowNumber, IEnumerable<double> elements)
+        {
+            var newValues = elements as double[] ?? elements.ToArray();
+            if (this.ColumnsCount != newValues.Count())
+            {
+                throw new Exception("Wrong number of items");
+            }
+            if (rowNumber >= this.RowsCount)
+            {
+                throw new Exception("Columns number exceeds existing columns");
+            }
+            for (var i = 0; i < this.ColumnsCount; i++)
+            {
+                this.matrix[rowNumber, i] = newValues[i];
+            }
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            return this.matrix.Cast<double>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public double GetSum()
+        {
+            return this.Aggregate(0.0, (sum, elem) => sum + elem);
+        }
+    }
+}
